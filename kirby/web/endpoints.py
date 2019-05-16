39fb0465466505dee8a8e7bf0874b2748ee478db
Pierre <pierre@adimian.com>
2019-05-16 15:53:53 +0200

    Adapt registration endpoint to the new model (now register a Script and not a Job anymore). Resolving models errors (set external_keys to the real primary_keys). Adding representation of rows of the model for readibility on the admin plateform.

--- kirby/web/endpoints.py
+++ kirby/web/endpoints.py
@@ -1,37 +1,58 @@
-import sqlalchemy
+from sqlalchemy import func
+from sqlalchemy.orm.exc import NoResultFound
 from flask import abort
+from datetime import datetime
 from flask_restplus import Api, Resource, reqparse
-from ..models import db, Job, JobType
+from ..models import db, Script, Topic
 
 api = Api()
 
 registration_parser = reqparse.RequestParser()
-registration_parser.add_argument("name", type=str, required=True)
-registration_parser.add_argument("description", type=str)
-registration_parser.add_argument(
-    "type", choices=["scheduled", "triggered"], required=True
-)
+registration_parser.add_argument("script_id", type=int)
+registration_parser.add_argument("source_id", type=int, action="append")
+registration_parser.add_argument("destination_id", type=int, action="append")
 
 
 @api.route("/registration")
 class Registration(Resource):
     @api.expect(registration_parser)
-    def post(self):
+    def patch(self):
         args = registration_parser.parse_args()
+
         try:
-            db.session.add(
-                Job(
-                    name=args["name"],
-                    description=args.get("description"),
-                    type=JobType(args["type"]),
-                )
+            # Update last_seen
+            script = (
+                db.session.query(Script).filter_by(id=args["script_id"]).one()
             )
+            script.last_seen = datetime.utcnow()
+
+            # Add sources and destinations
+            for source_id in args["source_id"]:
+                source = db.session.query(Topic).filter_by(id=source_id).one()
+                script.add_source(source)
+            for destination_id in args["destination_id"]:
+                destination = (
+                    db.session.query(Topic).filter_by(id=destination_id).one()
+                )
+                script.add_destination(destination)
+
             db.session.commit()
-            return {
-                "id": db.session.query(Job)
-                .filter_by(name=args["name"])
-                .one()
-                .id
-            }
-        except sqlalchemy.exc.IntegrityError:
-            abort(400, "There is already a Job with this name.")
+
+            return {"message": "Sucess"}
+        except NoResultFound:
+            abort(
+                400, "One or many of the id given refer to inexistent objects."
+            )
+
+
+topic_parser = reqparse.RequestParser()
+topic_parser.add_argument("name", type=str, required=True)
+
+
+@api.route("/topic")
+class TopicView(Resource):
+    @api.expect(topic_parser)
+    def get(self):
+        args = topic_parser.parse_args()
+        topic = db.session.query(Topic).filter_by(name=args["name"]).one()
+        return {"id": topic.id, "name": topic.name}
--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,24 +1,84 @@
 from requests_flask_adapter import Session
-from kirby.web import app_maker
 from pytest import fixture
+from datetime import datetime
+
+from kirby.web import app_maker
+from kirby.models import db, Environment, Job, JobType, Context, Script, Topic
 
 BASE_API = "http://some-test-server.somewhere"
 
 
+@fixture(scope="function")
+def webapp():
+    config = {
+        "TESTING": True,
+        "SQLALCHEMY_DATABASE_URI": "sqlite://",
+        "SQLALCHEMY_TRACK_MODIFICATIONS": False,
+    }
+    app = app_maker(config=config)
+    with app.app_context():
+        db.create_all()
+        yield app
+
+
 @fixture
-def session():
-    app = app_maker()
-    Session.register(BASE_API, app)
+def session(webapp):
+    Session.register(BASE_API, webapp)
     return Session()
 
 
-def test_it_register_a_job(session):
-    result = session.post(
+@fixture
+def db_populated():
+    env = Environment(name="test")
+    job = Job(
+        name="Booking",
+        description="This is a process for booking orders.",
+        type=JobType.TRIGGERED,
+    )
+    context = Context(environment=env, job=job)
+    script = Script(
+        package_name="booking_process",
+        package_version="1.0.1",
+        context=context,
+        first_seen=datetime.utcnow(),
+    )
+    orders_topic = Topic(name="Orders")
+    asset_management_topic = Topic(name="AssetManagement")
+
+    db.session.add_all(
+        [env, job, context, script, orders_topic, asset_management_topic]
+    )
+    db.session.commit()
+
+
+def get_id_topic(session, name):
+    return session.get(
+        "/".join([BASE_API, "topic"]), params={"name": name}
+    ).json()["id"]
+
+
+def test_if_get_id_topic(session, db_populated):
+    assert get_id_topic(session, "Orders") == 1
+
+
+def test_it_register_a_script(session, db_populated):
+    id_source = get_id_topic(session, "Orders")
+    id_destination = get_id_topic(session, "AssetManagement")
+
+    result = session.patch(
         "/".join([BASE_API, "registration"]),
         params={
-            "name": "Test Scheduled",
-            "description": "This is a test job.",
-            "type": "scheduled",
+            "script_id": 1,
+            "source_id": id_source,
+            "destination_id": id_destination,
         },
     )
     assert result.status_code == 200
+
+    script_registered = (
+        db.session.query(Script)
+        .filter_by(package_name="booking_process")
+        .one()
+    )
+    assert script_registered.sources[0].id == id_source
+    assert script_registered.destinations[0].id == id_destination
