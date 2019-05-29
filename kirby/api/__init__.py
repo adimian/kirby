33b9edbeadf1b4a3f042a15606158b394cd94d06
Pierre <pierre@adimian.com>
2019-05-29 15:36:40 +0200

    Merge branch 'master' into building-api

--- kirby/api/__init__.py
+++ kirby/api/__init__.py
@@ -1,6 +1,15 @@
 import requests
+import json
+import logging
+from pprint import pformat
+
+import msgpack
+from kafka import KafkaConsumer
+from smart_getenv import getenv
 
 from .context import ContextManager, ctx
+
+logger = logging.getLogger(__name__)
 
 
 class ServerError(BaseException):
@@ -57,3 +66,24 @@ class Kirby:
         self._register(
             destination_id=self.get_topic_id(destination.topic_name)
         )
+
+
+def read_topic(name):
+    bootstrap_servers = getenv(
+        "KAFKA_BOOTSTRAP_SERVERS", type=list, separator=","
+    )
+    consumer = KafkaConsumer(
+        name,
+        bootstrap_servers=bootstrap_servers,
+        auto_offset_reset="earliest",
+        enable_auto_commit=True,
+        value_deserializer=msgpack.loads,
+    )
+
+    for idx, message in enumerate(consumer, start=1):
+        message = message.value
+        try:
+            message = json.loads(message.decode("utf-8"))
+            logger.debug(pformat(message))
+        except Exception:
+            logger.debug(message)
