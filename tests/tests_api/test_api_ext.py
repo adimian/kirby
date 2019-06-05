d9c9930dec153633fc12df99e50f37ab32e073a1
PierreSavatte <pierre@adimian.com>
2019-06-05 17:21:42 +0200

    Now using MagicMock instead of a custom implementation

--- tests/tests_api/test_api_ext.py
+++ tests/tests_api/test_api_ext.py
@@ -1,50 +1,7 @@
-import os
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock
 import pytest
 
 from kirby.api.ext import WebClient, RETRIES, WAIT_BETWEEN_RETRIES
-
-
-class MockResult:
-    def __init__(self, status_code, json):
-        self.status_code = status_code
-        self._json = json
-        self.url = "http://false_url.just_to_mock"
-        self.text = str(self._json)
-
-    def json(self):
-        return self._json
-
-
-class MockSession:
-    def __init__(self, get=None, post=None):
-        if get:
-            self._return_get = get
-        else:
-            self._return_get = {}
-
-        if post:
-            self._return_post = post
-        else:
-            self._return_post = {}
-
-        self.data = {}
-
-    def get(self, key, *args, **kargs):
-        if self._return_get:
-            return self._return_get.pop(0)
-        elif self.data[key]:
-            return MockResult(200, self.data[key])
-        else:
-            raise RuntimeError(
-                "Your test is not good, you didn't give any data to return"
-            )
-
-    def post(self, key, *args, **kargs):
-        if self._return_post:
-            return self._return_post.pop(0)
-        else:
-            self.data[key] = kargs["data"]
 
 
 def test_it_creates_a_kirby_topic(kirby_topic):
@@ -64,7 +21,9 @@ def test_it_creates_a_kirby_topic(kirby_topic):
 def test_it_creates_a_kirby_ext_web_client(session_mock):
     data = {"foo": True}
 
-    session_mock.return_value = MockSession()
+    session_mock.return_value.get.return_value = MagicMock(
+        status_code=200, json=MagicMock(return_value=data)
+    )  # MockSession()
     with WebClient(
         "external_server", "http://some.external.server"
     ) as web_client:
@@ -81,18 +40,18 @@ def test_it_creates_a_kirby_ext_web_client(session_mock):
 def test_kirby_ext_web_client_handle_empty_response(session_mock):
     data = {"foo": True}
 
-    good_get = MockResult(status_code=200, json=data)
+    good_get = MagicMock(status_code=200, json=MagicMock(return_value=data))
 
     mocking_effects = [
-        MockResult(status_code=i, json={}) for i in [502, 504, 500, 501]
+        MagicMock(status_code=i, json=MagicMock(return_value={}))
+        for i in [502, 504, 500, 501]
     ]  # Errors
     mocking_effects.append(
-        MockResult(status_code=200, json={})
+        MagicMock(status_code=200, json=MagicMock(return_value={}))
     )  # Empty answer
 
     for mocking_effect in mocking_effects:
-
-        session_mock.return_value = MockSession(get=[mocking_effect, good_get])
+        session_mock.return_value.get.side_effect = [mocking_effect, good_get]
 
         with WebClient(
             "external_server", "http://some.external.server"
