0104f2ff8a39448fa1860b470cd682d89d4d9e75
PierreSavatte <pierre@adimian.com>
2019-06-18 14:09:51 +0200

    Adding context manager for populates a topic

--- kirby/api/ext.py
+++ kirby/api/ext.py
@@ -1,3 +1,4 @@
+import os
 import logging
 import datetime
 from urllib.parse import urljoin
@@ -5,6 +6,7 @@ import requests
 import msgpack
 from smart_getenv import getenv
 import tenacity
+from contextlib import contextmanager
 
 from kafka import KafkaConsumer, KafkaProducer
 from kafka.errors import NoBrokersAvailable
@@ -37,13 +39,9 @@ webserver_retry_args = {
 
 class Topic:
     def __init__(
-        self,
-        kirby_app,
-        topic_name_variable_name,
-        ssl_security_protocol=True,
-        testing=False,
+        self, kirby_app, topic_name, ssl_security_protocol=True, testing=False
     ):
-        self.name = kirby_app.ctx[topic_name_variable_name]
+        self.name = topic_name
         self.testing = testing
         self.ssl_security_protocol = ssl_security_protocol
         self.kirby_app = kirby_app
@@ -162,6 +160,32 @@ class Topic:
 
     def __next__(self):
         return self.next(timeout_ms=float("inf"))
+
+
+@contextmanager
+def topic_sender():
+    args = {
+        "client_id": "test_client",
+        "bootstrap_servers": os.environ["KAFKA_BOOTSTRAP_SERVERS"],
+        "value_serializer": msgpack.dumps,
+    }
+    if os.getenv("SECURITY_PROTOCOL"):
+        args.update(
+            {
+                "security_protocol": os.environ["SECURITY_PROTOCOL"],
+                "ssl_cafile": os.environ["KAFKA_SSL_CAFILE"],
+                "ssl_certfile": os.environ["KAFKA_SSL_CERTFILE"],
+                "ssl_keyfile": os.environ["KAFKA_SSL_KEYFILE"],
+            }
+        )
+    producer = KafkaProducer(**args)
+
+    def send(topic, data):
+        producer.send(topic, data)
+        producer.flush()
+
+    yield send
+    producer.close()
 
 
 class WebClient:
