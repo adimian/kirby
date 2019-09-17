ba4ad00e56724e0db6fa4689aa1f7f72a5a515b9
PierreSavatte <pierre@adimian.com>
2019-09-17 11:57:04 +0200

    Modifying the dummy package

---
+++ dummies/dummy/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+
+setup(
+    name="dummykirby",
+    packages=["dummykirby"],
+    version="0.0.0.dev",
+    license="MIT",
+    description="",
+    author="Kirby Team",
+    author_email="",
+    url="",
+    download_url="",
+    keywords=[],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
