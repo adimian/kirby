18ca71e11f54b1f4b4ae2c55a33c33441aa5ddbf
PierreSavatte <pierre@adimian.com>
2019-09-26 17:49:21 +0200

    Removing useless function

--- tests/test_executor/conftest.py
+++ tests/test_executor/conftest.py
@@ -2,7 +2,6 @@ import logging
 import os
 import pytest
 
-from pygit2 import Repository
 from tempfile import mkdtemp
 
 from kirby.supervisor.executor import parse_job_description
@@ -12,13 +11,6 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 DUMMY_PACKAGE_NAME = "dummykirby"
-
-
-def get_branch_name():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    kirby_dir = os.path.split(os.path.split(dir_path)[0])[0]
-    branch_full_name = Repository(kirby_dir).head.name
-    return branch_full_name.split("/")[-1]
 
 
 @pytest.fixture()
