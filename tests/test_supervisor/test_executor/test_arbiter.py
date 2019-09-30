852e0b7490afd716ad7d8b4663321828a8bed221
PierreSavatte <pierre@adimian.com>
2019-09-30 13:47:10 +0200

    Add test_arbiter. Refactor tests

---
+++ tests/test_supervisor/test_executor/test_arbiter.py
@@ -0,0 +1,32 @@
+import logging
+import os
+import pytest
+
+from kirby.supervisor.executor import ProcessState
+from kirby.supervisor.executor.arbiter import Arbiter
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+
+@pytest.mark.skipif(
+    not os.getenv("PIP_EXTRA_INDEX_URL"),
+    reason=(
+        f"You haven't set any extra index for pip. "
+        "Make sure you host the package somewhere."
+    ),
+)
+def test_arbiter_job_reraised_if_failed(venv_directory, queue_for_arbiter):
+    arbiter = Arbiter(_queue=queue_for_arbiter)
+
+    # Loop until the process fail
+    while arbiter.status != ProcessState.FAILED:
+        pass
+
+    # Loop until the process is re-raised
+    while arbiter.status == ProcessState.FAILED:
+        pass
+
+    assert arbiter.status == ProcessState.RUNNING
+
+    arbiter.kill()
--- tests/test_supervisor/test_executor/test_runner.py
+++ tests/test_supervisor/test_executor/test_runner.py
@@ -2,27 +2,12 @@ import logging
 import os
 import pytest
 
-from smart_getenv import getenv
-
-from kirby.api.queue import Queue
 from kirby.supervisor.executor import ProcessState
 from kirby.supervisor.executor.runner import Runner
 
 
 logger = logging.getLogger(__name__)
-
 logging.basicConfig(level=logging.DEBUG)
-
-RUNNER_TOPIC_NAME = getenv(
-    "KIRBY_TOPIC_JOB_OFFERS", type=str, default=".kirby.job-offers"
-)
-
-
-@pytest.fixture
-def queue_for_runner(single_job_description):
-    queue = Queue(RUNNER_TOPIC_NAME, testing=True)
-    queue.send(single_job_description)
-    return queue
 
 
 @pytest.mark.skipif(
