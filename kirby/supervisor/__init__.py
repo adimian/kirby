dca3d79833c6e657a943590a3f5abfaa09be7312
PierreSavatte <pierre@adimian.com>
2019-10-07 13:07:01 +0200

    Scheduler now ask for supervisors to list their arbiters.

--- kirby/supervisor/__init__.py
+++ kirby/supervisor/__init__.py
@@ -1,34 +1,81 @@
+import datetime
 import logging
-from time import perf_counter
-from time import sleep
 
 from redis import Redis
 from smart_getenv import getenv
+from time import perf_counter, sleep
 
+from kirby.models import JobType
+from kirby.api.ext.topic import earliest_kafka_date
 from kirby.api.queue import Queue
 from kirby.supervisor.election import Election
-from kirby.supervisor.scheduler import Scheduler
+from kirby.supervisor.scheduler import Scheduler, MessageType
 from kirby.supervisor.executor.runner import Runner
 from kirby.supervisor.executor.arbiter import Arbiter
 
 logger = logging.getLogger(__name__)
 
+USE_TLS = getenv("KAFKA_USE_TLS", type=bool, default=False)
+JOB_OFFERS_TOPIC_NAME = getenv(
+    "KIRBY_TOPIC_JOB_OFFERS", type=str, default=".kirby.job-offers"
+)
+RUNNING_DEAMONS_TOPIC_NAME = getenv(
+    "KIRBY_TOPIC_JOB_OFFERS", type=str, default=".kirby.job-offers"
+)
 
-def run_supervisor(name, window, wakeup, nb_runner, nb_arbiter):
+
+def send_arbiters_job_if_needed(
+    queue_list_arbiters, arbiters, last_update_message
+):
+    if last_update_message:
+        start = last_update_message.timestamp
+    else:
+        start = earliest_kafka_date()
+
+    end = datetime.datetime.utcnow()
+
+    # Search for new_update_message
+    new_update_message = None
+    for message in queue_list_arbiters.rewind(earlier=start, latest=end):
+        if message.headers["type"] == MessageType.UPDATE.value:
+            new_update_message = message
+            break
+
+    if new_update_message:
+        last_update_message = new_update_message
+        for arbiter in arbiters:
+            queue_list_arbiters.append(
+                arbiter.job.json_repr(),
+                headers={"type": MessageType.JOB.value},
+            )
+
+    return last_update_message
+
+
+def run_supervisor(name, window, wakeup, nb_runner):
     server = Redis()
-    queue = Queue(
-        name=getenv(
-            "KIRBY_TOPIC_JOB_OFFERS", type=str, default=".kirby.job-offers"
-        ),
-        use_tls=getenv("KAFKA_USE_SSL", type=bool, default=False),
+
+    queue_job_offers_arbiters = Queue(
+        name=JOB_OFFERS_TOPIC_NAME, use_tls=USE_TLS, group_id=".kirby.arbiters"
     )
-    scheduler = Scheduler(queue=queue, wakeup=wakeup)
+    queue_job_offers_runners = Queue(
+        name=JOB_OFFERS_TOPIC_NAME, use_tls=USE_TLS, group_id=".kirby.runners"
+    )
+    queue_running_deamons = Queue(
+        name=RUNNING_DEAMONS_TOPIC_NAME, use_tls=USE_TLS
+    )
+
+    scheduler = Scheduler(
+        queue_job_offers=queue_job_offers_arbiters,
+        queue_running_deamons=queue_running_deamons,
+        wakeup=wakeup,
+    )
 
     for i in range(nb_runner):
-        Runner(queue=scheduler.queue)
-    for i in range(nb_arbiter):
-        Arbiter(queue=scheduler.queue)
+        Runner(queue_job_offers_runners)
 
+    arbiters = []
+    last_update_message = None
     with Election(identity=name, server=server, check_ttl=window) as me:
         while True:
             checkpoint = perf_counter()
@@ -36,11 +83,25 @@ def run_supervisor(name, window, wakeup, nb_runner, nb_arbiter):
                 content = scheduler.fetch_jobs()
                 if content is not None:
                     jobs = scheduler.parse_jobs(content)
+                    running_arbiters_job = scheduler.get_running_arbiters_job(
+                        nb_supervisors
+                    )
                     for job in jobs:
-                        scheduler.queue_job(job)
+                        if job.type == JobType.SCHEDULED or (
+                            job.type == JobType.DAEMON
+                            and (job.package_name not in running_arbiters_job)
+                        ):
+                            if job.type == JobType.DAEMON:
+                                arbiters.append(
+                                    Arbiter(queue_job_offers_arbiters)
+                                )
+                            scheduler.queue_job(job)
 
             else:
                 logger.debug("not the leader, do nothing")
+                last_update_message = send_arbiters_job_if_needed(
+                    queue_running_deamons, arbiters, last_update_message
+                )
 
             drift = perf_counter() - checkpoint
             next_wakeup = wakeup - drift
--- kirby/supervisor/scheduler.py
+++ kirby/supervisor/scheduler.py
@@ -1,19 +1,26 @@
 import datetime
 import json
-
+import logging
 import requests
+
+from enum import Enum
 from smart_getenv import getenv
 
-import logging
-
-from ..exc import CoolDownException
+from kirby.exc import CoolDownException
 
 logger = logging.getLogger(__name__)
 
 
+class MessageType(Enum):
+    UPDATE = "update"
+    JOB = "job"
+    DONE = "done"
+
+
 class Scheduler:
-    def __init__(self, queue, wakeup):
-        self.queue = queue
+    def __init__(self, wakeup, queue_job_offers, queue_running_deamons):
+        self.queue_job_offers = queue_job_offers
+        self.queue_running_deamons = queue_running_deamons
         self.cooldown = datetime.timedelta(seconds=wakeup)
 
     def fetch_jobs(self):
@@ -36,9 +43,44 @@ class Scheduler:
         if now is None:
             now = datetime.datetime.utcnow()
 
-        submitted_jobs = self.queue.between(start=now - self.cooldown, end=now)
+        submitted_jobs = self.queue_job_offers.between(
+            start=now - self.cooldown, end=now
+        )
 
         if job in submitted_jobs:
             raise CoolDownException()
         else:
-            self.queue.append(job, submitted=now)
+            self.queue_job_offers.append(job, submitted=now)
+
+    def _ask_update_on_running_arbiters_job(self):
+        self.queue_running_deamons.send(
+            "update", headers={"type": MessageType.UPDATE.value}
+        )
+
+    def get_running_arbiters_job(
+        self, nb_supervisors, ask_update=True, earlier_rewind=None
+    ):
+        if ask_update:
+            self._ask_update_on_running_arbiters_job()
+
+        nb_supervisors_done = 0
+        packages = []
+        for message in self.queue_running_deamons.rewind(
+            earlier=earlier_rewind
+        ):
+            msg_type = message.headers["type"]
+            if msg_type != MessageType.UPDATE.value:
+                if msg_type == MessageType.DONE.value:
+                    nb_supervisors_done += 1
+                elif msg_type == MessageType.JOB.value:
+                    packages.insert(0, message.value)
+            else:
+                if nb_supervisors_done != nb_supervisors:
+                    logging.warning(
+                        f"The list of running arbiters may be wrong: The "
+                        "number of supervisors listed is not equal as the "
+                        "number of arbiters announced."
+                    )
+                break
+
+        return packages
