16c3461abb9b79f656f6c089fc29755d18c1f98f
PierreSavatte <pierre@adimian.com>
2019-10-15 15:55:20 +0200

    Delete args modification

--- kirby/api/queue.py
+++ kirby/api/queue.py
@@ -1,6 +1,5 @@
 import logging
 
-from .context import ctx
 from .ext.topic import Topic
 
 logger = logging.getLogger(__name__)
@@ -8,8 +7,6 @@ logger = logging.getLogger(__name__)
 
 class Queue(Topic):
     def __init__(self, name, *args, **kargs):
-        if not kargs.get("testing", False):
-            kargs.update(group_id=ctx.KIRBY_SUPERVISOR_GROUP_ID)
         super().__init__(topic_name=name, *args, **kargs)
 
     def append(self, *args, **kargs):
