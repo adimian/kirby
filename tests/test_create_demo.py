5e81e0cd850dbc6007761e6a38b0b330ca0adc3a
PierreSavatte <pierre@adimian.com>
2019-10-28 18:04:12 +0100

    Adapt test to new file

--- tests/test_create_demo.py
+++ tests/test_create_demo.py
@@ -7,7 +7,7 @@ from kirby.models import db
 from kirby.web import app_maker
 
 DEMO_JSON_FILE_PATH = os.path.join(
-    os.path.dirname(__file__), "..", "example", "short_demo.json"
+    os.path.dirname(__file__), "..", "example", "demo.json"
 )
 
 
@@ -15,7 +15,7 @@ def test_read_json():
     env_dict, env_var_dict, job_dict, ext_list, user_list, notification_group_dict = read_json(
         DEMO_JSON_FILE_PATH
     )
-    assert env_dict == {"dev": "Development"}
+    assert env_dict == {"test": "Test", "dev": "Development", "prod": "Prod"}
     assert env_var_dict == {
         models.ConfigScope.GLOBAL: {
             "KAFKA_BOOTSTRAP_SERVERS": "localhost:9092",
@@ -23,52 +23,145 @@ def test_read_json():
         }
     }
     assert job_dict == {
-        "sensorcash": {
-            "description": "cashregister daemon (from bakery API to cashregister topic)",
+        "sensor_sales": {
+            "description": "Fetch sales",
             "type": models.JobType.DAEMON,
-            "package_version": {"dev": "0.1"},
+            "package_version": {
+                "test": "0.0.1",
+                "dev": "0.1.0",
+                "prod": "1.0.0",
+            },
+            "notification_group": "sysadmins",
+            "config": {
+                models.ConfigScope.JOB: {"SALES_TOPIC_NAME": "sales"},
+                models.ConfigScope.CONTEXT: {
+                    "test": {"SALES_API_BASE": "localhost:8000"},
+                    "dev": {
+                        "SALES_API_BASE": "http://some.server.somewhere:8000"
+                    },
+                    "prod": {
+                        "SALES_API_BASE": "http://some.server.elsewhere:8000"
+                    },
+                },
+            },
+        },
+        "sales_to_prevision": {
+            "description": "Process sales to estimate prevision",
+            "type": models.JobType.DAEMON,
+            "package_version": {
+                "test": "0.0.1",
+                "dev": "0.1.0",
+                "prod": "1.0.0",
+            },
             "notification_group": "sysadmins",
             "config": {
                 models.ConfigScope.JOB: {
-                    "CASHREGISTER_TOPIC_NAME": "cashregister",
-                    "BAKERY_API_BASE": "http://127.0.0.1:8000",
-                    "CASHREGISTER_ENDPOINT": "sales/cashregister",
+                    "SALES_TOPIC_NAME": "sales",
+                    "PREVISION_TOPIC_NAME": "prevision",
                 },
                 models.ConfigScope.CONTEXT: {
-                    "dev": {},
                     "test": {},
+                    "dev": {},
                     "prod": {},
                 },
             },
         },
-        "realtime": {
-            "description": "create total files (from  cashregister topic to totalfiles)",
+        "prevision_to_production": {
+            "description": "Set production as prevision",
             "type": models.JobType.SCHEDULED,
-            "package_version": {"dev": "0.1"},
-            "notification_group": "sysadmins",
             "scheduled_param": {
-                "name": "every two minutes",
+                "name": "schedule for prevision_to_production",
                 "hour": "*",
-                "minute": "*/2",
+                "minute": "*",
             },
+            "package_version": {
+                "test": "0.0.1",
+                "dev": "0.1.0",
+                "prod": "1.0.0",
+            },
+            "notification_group": "sysadmins",
             "config": {
                 models.ConfigScope.JOB: {
-                    "CASHREGISTER_TOPIC_NAME": "cashregister",
-                    "TOTAL_FILES_FOLDER_PATH": "realtime_totals",
+                    "PREVISION_TOPIC_NAME": "prevision",
+                    "PRODUCTION_TOPIC_NAME": "production",
                 },
                 models.ConfigScope.CONTEXT: {
-                    "dev": {},
                     "test": {},
+                    "dev": {},
                     "prod": {},
+                },
+            },
+        },
+        "parse_surplus": {
+            "description": "Compare production and sales to get the surplus",
+            "type": models.JobType.SCHEDULED,
+            "scheduled_param": {
+                "name": "schedule for parse_surplus",
+                "hour": "*",
+                "minute": "*",
+            },
+            "package_version": {
+                "test": "0.0.1",
+                "dev": "0.1.0",
+                "prod": "1.0.0",
+            },
+            "notification_group": "sysadmins",
+            "config": {
+                models.ConfigScope.JOB: {
+                    "PRODUCTION_TOPIC_NAME": "production",
+                    "SALES_TOPIC_NAME": "sales",
+                    "SURPLUS_TOPIC_NAME": "surplus",
+                },
+                models.ConfigScope.CONTEXT: {
+                    "test": {"STOCK_API_BASE": "localhost:8000"},
+                    "dev": {
+                        "STOCK_API_BASE": "http://some.server.somewhere:8000"
+                    },
+                    "prod": {
+                        "STOCK_API_BASE": "http://some.server.elsewhere:8000"
+                    },
+                },
+            },
+        },
+        "get_profit": {
+            "description": "Calculates profit from sales, production and surplus",
+            "type": models.JobType.DAEMON,
+            "package_version": {
+                "test": "0.0.1",
+                "dev": "0.1.0",
+                "prod": "1.0.0",
+            },
+            "notification_group": "sysadmins",
+            "config": {
+                models.ConfigScope.JOB: {
+                    "PRODUCTION_TOPIC_NAME": "production",
+                    "SALES_TOPIC_NAME": "sales",
+                },
+                models.ConfigScope.CONTEXT: {
+                    "test": {"PROFIT_API_BASE": "localhost:8000"},
+                    "dev": {
+                        "PROFIT_API_BASE": "http://some.server.somewhere:8000"
+                    },
+                    "prod": {
+                        "PROFIT_API_BASE": "http://some.server.elsewhere:8000"
+                    },
                 },
             },
         },
     }
-    assert ext_list == ["bakery", "cashregister", "totalfiles"]
+    assert ext_list == [
+        "DB/Sales",
+        "DB/Stock",
+        "DB/Profit",
+        "sales",
+        "prevision",
+        "production",
+        "surplus",
+    ]
     assert user_list == [
         {"username": "demo", "password": "demo", "role": "admin"}
     ]
-    assert notification_group_dict == {"sysadmins": ["alice@local.local"]}
+    assert notification_group_dict == {"sysadmins": []}
 
 
 def test_create_demo_db():
